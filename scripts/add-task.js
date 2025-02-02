ba85061d5d2b28160704e6f0697ec4bffbe35092
DominikTroendle <domi.troendle@gmail.com>
2025-02-02 13:19:02 +0100

    added functionality for prio buttons

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -1,9 +1,23 @@
 // add assigned-to dropdown onclick
 
-// change color of button onclick
 function prioButtonSelected(prio) {
+    clearPrioButtons();
     let button = document.getElementById(`${prio}`);
-    button.classList.toggle(`${prio}`);
+    let svg = document.getElementById(`svg-${prio}`);
+    button.classList.add(`${prio}`);
+    button.classList.add('white');
+    svg.classList.add('filter-white');
+}
+
+function clearPrioButtons() {
+    let prios = ["urgent", "medium", "low"];
+    for (let i = 0; i < prios.length; i++) {
+        let button = document.getElementById(`${prios[i]}`);
+        let svg = document.getElementById(`svg-${prios[i]}`);
+        button.classList.remove(`${prios[i]}`);
+        button.classList.remove('white');
+        svg.classList.remove('filter-white');
+    }
 }
 
 // change arrow direction of assigned-to & category onclick
