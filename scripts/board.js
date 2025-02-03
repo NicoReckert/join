18e9758195efe842d1970586822ef59a85e34f31
Nico Reckert <n.r-86@gmx.de>
2025-02-03 19:57:03 +0100

    create a small card template

--- scripts/board.js
+++ scripts/board.js
@@ -1,4 +1,19 @@
+let currentDraggedElement;
+
 function changeImgSource(id, imgSource) {
     imgId = document.getElementById(id)
     imgId.src = imgSource;
 }
+
+function startDragging(id) {
+    currentDraggedElement = id;
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function moveTo(category) {
+
+}
+
---
+++ templates/board-template.js
@@ -0,0 +1,23 @@
+function smallCardTemplate(id) {
+    return `<div class="user-story__box" id="${id}" draggable="true" ondragstart="startDragging(${id})">
+                <div class="user-story__category-box">
+                    <span class="user-story__category-text">User Story</span>
+                </div>
+                <span class="user-story__title">Kochwelt Page & Recipe Recommender</span>
+                <span class="user-story__discription">Build start page with recipe recommendation...</span>
+                <div class="user-story__scale-text-box">
+                    <div class="user-story__subtask-scale-box">
+                        <div class="user-story__subtask-scale-fill"></div>
+                    </div>
+                    <span class="user-story__subtask-text">1/2 Subtasks</span>
+                </div>
+                <div class="user-story__name-priority-box">
+                    <div class="user-story__name-box">
+                        <span class="user-story__name background-color-1">AM</span>
+                        <span class="user-story__name background-color-2">EM</span>
+                        <span class="user-story__name background-color-3">MB</span>
+                    </div>
+                    <img class="user-story__img" src="assets/icons/Priority symbols.png" alt="">
+                </div>
+            </div>`
+}
