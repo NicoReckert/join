e516ad00b2d7d6044438e172fd31fbdf5122c6a8
DominikTroendle <domi.troendle@gmail.com>
2025-02-04 19:54:56 +0100

    designed contacts preview section

---
+++ scripts/contacts.js
@@ -0,0 +1,62 @@
+let contacts = [
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    },
+    {
+        "name": "Anton Mayer",
+        "mail": "antom@gmail.com"
+    }
+];
+
+function selectContact() {
+    let element = document.getElementById('')
+}
