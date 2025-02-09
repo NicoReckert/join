cef02465894830d080b56dce5b68b75d0448c6cc
Nico Reckert <n.r-86@gmx.de>
2025-02-09 11:29:22 +0100

    small changes in script.js, style.css and other

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function addClassFocusMenuButton(id) {
+function addClassSelectedMenuButton(id) {
     let currentMenuButton = document.getElementById(id);
     currentMenuButton.classList.add("menu-button-selected");
+}
+
+function addClassSelectedMenuBottomButton(id) {
+    let currentMenuBottomButton = document.getElementById(id);
+    currentMenuBottomButton.classList.add("menu-button-bottom-selected");
 }
