4a485de659e370937e4a6815cd8e51a82578c998
Kevin-Developer-B <kevin_breiter@web.dem>
2025-02-10 08:43:42 +0100

    added the saveMenuId and the toLastClickedMenu function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function addClassSelectedMenuButton(id) {
     let currentMenuButton = document.getElementById(id);
     currentMenuButton.classList.add("menu-button-selected");
+    saveMenuId(id)
 }
 
 function addClassSelectedMenuBottomButton(id) {
@@ -14,4 +15,34 @@ function accountClickMenu() {
 
     overlay.classList.toggle('active');
     menuBox.classList.toggle('inactive');
+}
+
+function saveMenuId(menuId) {
+    localStorage.setItem('lastClickedMenu', menuId);
+    console.log('Gespeicherte Menü-ID: ' + menuId);
+}
+
+function toLastClickedMenu() {
+    const lastClickedMenu = localStorage.getItem('lastClickedMenu');
+    if (lastClickedMenu) {
+        let targetUrl;
+            switch(lastClickedMenu) {
+            case 'summary-menu-button':
+                targetUrl = 'summary.html';
+                break;
+            case 'add-task-menu-button':
+                targetUrl = 'add-task.html';
+                break;
+            case 'board-menu-button':
+                targetUrl = 'board.html';
+                break;
+            case 'contacts-menu-button':
+                targetUrl = 'contacts.html';
+                break;
+            default:
+                targetUrl = 'summary.html';
+                break;
+        }
+        window.location.href = targetUrl;
+    }
 }
