58ba052e93aa422c102930aeeeea52f9a78c78fa
Kevin-Developer-B <kevin_breiter@web.dem>
2025-02-10 12:15:48 +0100

    added a new contect function in contect.html

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,3 +1,14 @@
 function selectContact() {
     let element = document.getElementById('')
 }
+
+
+function addNewContect() {
+    refOverlay = document.getElementById('nexContectOverlay');
+    refOverlay.classList.toggle('d-none')
+    refOverlay.onclick = function(event) {
+        if (event.target === refOverlay) {
+            refOverlay.classList.add('d-none');
+        }
+    };
+}
