446e65517e3598347e5469db097693b55623a8d1
Kevin-Developer-B <kevin_breiter@web.dem>
2025-02-12 07:49:22 +0100

    added a color initial in the contact.js

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,13 +1,20 @@
 const BASE_URL = "https://join-skizze-default-rtdb.europe-west1.firebasedatabase.app/"
 let allContacts = [];
 let contactNames = [];
+const letterColors = {
+    "A": "bg-blue", "B": "bg-orange", "C": "bg-pink", "D": "bg-bluepurple",
+    "E": "bg-purple", "F": "bg-skyblue", "G": "bg-turquoise", "H": "bg-redorange",
+    "I": "bg-peach", "J": "bg-rose", "K": "bg-sun", "L": "bg-darkblue",
+    "M": "bg-green", "N": "bg-yellow", "O": "bg-red", "P": "bg-darkyellow",
+    "Q": "bg-blue", "R": "bg-orange", "S": "bg-pink", "T": "bg-bluepurple",
+    "U": "bg-purple", "V": "bg-skyblue", "W": "bg-turquoise", "X": "bg-redorange",
+    "Y": "bg-peach", "Z": "bg-rose"
+};
 
 async function init() {
     await loadAllUserContacts();
     await allUserContacts();
     await loadContactList();
-    await findName();
-    
     // await selectMoreContactInformation()
 }
 
@@ -83,45 +90,73 @@ async function allUserContacts() {
 }
 
 async function loadContactList() {
-    let refContent = document.getElementById('contactList')
-    refContent.innerHTML = "";
+    let contactList = document.getElementById('contactList');
+    contactList.innerHTML = "";
+    allContacts.sort((a, b) => a.name.localeCompare(b.name));
+    let currentLetter = "";
     for (let i = 0; i < allContacts.length; i++) {
-        refContent.innerHTML += await getContactListTemplate(i);
-    } 
+        let contactName = allContacts[i].name;
+        let contactMail = allContacts[i].email;
+        let firstLetter = contactName.charAt(0).toUpperCase();
+        let isNewGroup = currentLetter !== firstLetter;
+        if (isNewGroup) {
+            currentLetter = firstLetter;
+            contactList.innerHTML += `<div">
+                <div class="container-letter">${currentLetter}</div>
+                <div id="group-${currentLetter}"></div>
+            </div>`;
+        }
+        let groupContainer = document.getElementById(`group-${currentLetter}`);
+        let isFirstContactInGroup = groupContainer.children.length === 0;
+        let bgColorClass = getBackgroundColor(contactName);
+
+        groupContainer.innerHTML += `
+            ${isNewGroup || isFirstContactInGroup ? "<hr>" : ""}
+            ${await getContactListTemplate(contactName, contactMail, bgColorClass)}
+        `;
+        document.getElementById(`${contactName}`).innerText = contactName;
+        document.getElementById(`doppelInitials-${contactName}`).innerText = findInitials(contactName);
+    }
 }
 
-function showInitials() {
-    
+function getBackgroundColor(contactName) {
+    let firstLetter = contactName.charAt(0).toUpperCase();
+    return letterColors[firstLetter] || "bg-grey";
 }
+
+function findInitials(contactName) {
+    let name = contactName.split(' ');
+    let initials = '';
+    for (let i = 0; i < name.length; i++) {
+        initials += name[i].substring(0, 1).toUpperCase();
+    }
+    return initials
+}
+
 
 function selectContact() {
     let element = document.getElementById('selectContact');
     element.classList.toggle('select-contact');
-    
     if (element) {
         element.style.color = element.style.color === "white" ? "black" : "white";
     }
 }
 
-async function selectMoreContactInformation() {
-    let refMoreInfo = document.getElementById('moreInformationContact')
-    for (let i = 0; i < allContacts.length; i++) {
-        refMoreInfo.innerHTML += await selectMoreContactInformationTemplate(i);
-    }
-}
+// async function selectMoreContactInformation() {
+//     let refMoreInfo = document.getElementById('moreInformationContact')
+//     for (let i = 0; i < allContacts.length; i++) {
+//         refMoreInfo.innerHTML += await selectMoreContactInformationTemplate(i);
+//     }
+// }
 
-async function findName() {
-    for (let i = 0; i < allContacts.length; i++) {
-        contactNames.push({
-            name : allContacts[i].name
-        })
-    }
-    console.log(contactNames);
-}
-
-async function findInitialsColors() {
-    
-}
+// async function findName() {
+//     for (let i = 0; i < allContacts.length; i++) {
+//         contactNames.push({
+//             name : allContacts[i].name
+//         })
+//     }
+//     console.log(contactNames);
+// }
 
 
 
--- templates/contacts-template.js
+++ templates/contacts-template.js
@@ -1,31 +1,29 @@
-async function getContactListTemplate(i) {
+async function getContactListTemplate(name, email, bgColorClass) {
 return `
     <div>
-        <div class="container-letter">
-            <span>A</span>
-        </div>
-        <hr>
         <div class="container-contact" onclick="selectContact()" id="selectContact">
-            <div class="container-initials">AM</div>
+            <div class="container-initials ${bgColorClass}" id="doppelInitials-${name}">AM</div>
                 <div class="container-contact-preview">
-                    <span class="contact-preview-name">${allContacts[i].name}</span>
-                    <span class="contact-preview-mail">${allContacts[i].email}</span>
+                    <span class="contact-preview-name" id="${name}">${name}</span>
+                    <span class="contact-preview-mail">${email}</span>
                 </div>
             </div>
+        </div>
+    </div>
 `
 }
 
-async function selectMoreContactInformationTemplate(i){
-return `
-    <section>
-        <div>
-            <div class="more-info-initials">
-                <h1>AM</h1>
-            </div>
-            <div>
-                <h1>${allContacts[i].name}</h1>
-            </div>
-        </div>
-    </section>
-`
-}
+// async function selectMoreContactInformationTemplate(i){
+// return `
+//     <section>
+//         <div>
+//             <div class="more-info-initials">
+//                 <h1>AM</h1>
+//             </div>
+//             <div>
+//                 <h1>${allContacts[i].name}</h1>
+//             </div>
+//         </div>
+//     </section>
+// `
+// }
