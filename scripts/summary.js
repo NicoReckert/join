c713ab40446ab739557de35adeabedf8589941f7
Kevin-Developer-B <kevin_breiter@web.dem>
2025-02-20 10:13:07 +0100

    added the current geeting

--- scripts/summary.js
+++ scripts/summary.js
@@ -3,9 +3,25 @@ function changeImgSource(id, imgSource) {
     imgId.src = imgSource;
 }
 
+function init() {
+    currentDate();
+    currentTime();
+}
+
 function currentDate() {
     const today = new Date();
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const dateString = today.toLocaleDateString('en-US', options);
     document.getElementById("currentDate").innerText = dateString;
+}
+
+function currentTime() {
+    const currentTime = new Date().getHours();
+    if (currentTime < 12) {
+        document.getElementById('currentGreeting').innerHTML = "Good morning"
+    } else if (currentTime < 18) {
+        document.getElementById('currentGreeting').innerHTML = "Good day"
+    } else {
+        document.getElementById('currentGreeting').innerHTML = "Good evening"
+    }
 }
