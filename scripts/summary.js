9ccad74d03816ab0e623e7f10e9ea92c5db22834
Kevin-Developer-B <kevin_breiter@web.dem>
2025-02-20 09:46:35 +0100

    added the current date in summary.html

--- scripts/summary.js
+++ scripts/summary.js
@@ -2,3 +2,10 @@ function changeImgSource(id, imgSource) {
     imgId = document.getElementById(id)
     imgId.src = imgSource;
 }
+
+function currentDate() {
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const dateString = today.toLocaleDateString('en-US', options);
+    document.getElementById("currentDate").innerText = dateString;
+}
